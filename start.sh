c0bb8a84d875061022334286277b8a312075d92a
Valery Kotlarov <valery@seekingalpha.com>
2018-09-30 13:19:10 +0300

    Init

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SESSION=$USER
+
+tmux -2 new-session -d -s $SESSION
+
+# Setup a window for tailing log files
+tmux split-window -v
+tmux select-pane -t 0
+tmux send-keys "ruby -run -e httpd -- -p 5000 ." C-m
+tmux select-pane -t 1
+tmux resize-pane -U 8
+tmux send-keys "scss --watch css/style.scss:css/style.css" C-m
+tmux split-window -v
+tmux select-pane -t u
+tmux send-keys "filewatcher 'tut1.slim' 'slimrb tut1.slim > tut1.html'" C-m
+
+# Set default window
+tmux select-window -t $SESSION:1
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
