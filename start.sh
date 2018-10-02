d936a7ae8c15f7350984894b2c8ba33145802b4c
Valery Kotlarov <valery@seekingalpha.com>
2018-10-02 09:43:01 +0300

    Add dependency

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ tmux select-pane -t 0
 tmux send-keys "ruby -run -e httpd -- -p 5000 ." C-m
 tmux select-pane -t 1
 tmux resize-pane -U 8
-tmux send-keys "scss --watch css/style.scss:css/style.css css/common.scss:css/common.css" C-m
+tmux send-keys "scss --watch css/style.scss:css/style.css css/style-tut2.scss:css/style-tut2.css css/common.scss:css/common.css" C-m
 tmux split-window -v
 tmux select-pane -t u
 tmux send-keys "filewatcher 'tut1.slim' 'slimrb tut1.slim > tut1.html'" C-m
